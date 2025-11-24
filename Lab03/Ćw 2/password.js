de15f3582046cbf0e9cd48bb2595e05e98aebc0c
TJakubCode <jakub.tu7@wp.lp>
2025-11-24 18:13:16 +0100

    Initial commit of Lab03

---
+++ Lab03/Ćw 2/password.js
@@ -0,0 +1,7 @@
+const upperCase = document.getElementById("pass-upper-case");
+const specialChars = document.getElementById("pass-special-chars");
+const minLength = document.getElementById("pass-min-length");
+const maxLegth = document.getElementById("pass-max-length");
+const submit = document.getElementById("sumbit");
+
+submit.addEventListener("click", () => {});
